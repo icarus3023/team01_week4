39ffcf5a729890dbfbf0b2c6656e87d53cf9d5e6
icarus3023 <sample3023@gmail.com>
2016-04-09 13:33:10 +0900

    add testcalminutegoldexistaddline

--- src/test/java/team01/week04/Testing.java
+++ src/test/java/team01/week04/Testing.java
@@ -15,32 +15,38 @@ public class Testing {
 
 	@Test
 	public void testcalline(){ // 회선으로만 추가되는 요금 test
-		Calculator cal2 = new Calculator();//assertEquals(a,b)
-		cal2.grade = 1;
-		cal2.line = 4;
+		Calculator cal = new Calculator();//assertEquals(a,b)
+		cal.grade = 1;
+		cal.line = 4;
 		assertTrue(cal2.calline()==34);
 	}
 	@Test
 	public void testonelinegoldcalline(){
-		Calculator cal1 = new Calculator(); //assertEquals(a,b)
-		cal1.grade = 1;
-		cal1.line = 1;
+		Calculator cal = new Calculator(); //assertEquals(a,b)
+		cal.grade = 1;
+		cal.line = 1;
 		assertTrue(cal1.calline()==0.0);	
 	}
 	@Test
-	public void testcalminute(){
-		Calculator cal3 = new Calculator();//assertEquals(a,b)
-		cal3.grade = 1;
-		cal3.time = 878;
+	public void testcalminutegoldnoaddline(){
+		Calculator cal = new Calculator();//assertEquals(a,b)
+		cal.grade = 1;
+		cal.time = 878;
 		assertTrue((cal3.calminute()-49.95)<0.1&&(cal3.calminute()-49.95)>=0);
 	}
-
+	@Test
+	public void testcalminutegoldexistaddline(){
+		Calculator cal = new Calculator();//assertEquals(a,b)
+		cal.grade = 1;
+		cal.time = 1127;
+		assertTrue((cal.calminute()-107.1)<0.1&&(cal.calminute()-107.1)>=0);
+	}
 	@Test
 	public void testmoney(){		
-		Calculator cal4 = new Calculator();//assertEquals(a,b)
-		cal4.grade = 2;
-		cal4.line = 2;
-		cal4.time = 523;
+		Calculator cal = new Calculator();//assertEquals(a,b)
+		cal.grade = 2;
+		cal.line = 2;
+		cal.time = 523;
 		int temp = (int)cal4.money();
 		assertTrue(temp==63);
 	}
